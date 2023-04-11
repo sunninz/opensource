165cbc14a75d6d81f16118bb06f2f045f7f52cac
sunninz <129724474+sunninz@users.noreply.github.com>
2023-04-12 00:27:16 +0900

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,52 @@
+# get two integer parameters
+# return sum
+def plus(x, y):
+    return x+y
+
+def minus(x, y):
+    return x-y
+
+def mul(x, y):
+    return x*y
+
+def div(x, y):
+    return x/y
+
+# main function
+def main():
+    check = 1
+    print("Welcome to calcuator")
+    while check >= 1:        
+        print("0: exit, 1: plus, 2: minus, 3: multiple, 4: division")
+        check = int(input())
+        if check == 1:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", plus(x,y))
+        elif check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", minus(x,y))
+        elif check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", mul(x,y))
+        elif check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", div(x,y))    
+        elif check > 4:
+            print("Unsupported")
+        else:
+            print("Thank you")
+
+if __name__ == "__main__":
+    main()
